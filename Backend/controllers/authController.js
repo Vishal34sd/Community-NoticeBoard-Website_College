c53748b01111e751cc320169c379069f67da3816
vishal-07 <dubeyvishal714@gmail.com>
2025-08-04 01:03:17 +0530

    register user created

--- Backend/controllers/authController.js
+++ Backend/controllers/authController.js
@@ -1,11 +1,46 @@
 import User from "../model/userSchema.js";
+import bcrypt from "bcrypt";
 
 
 const registerUser = async(req , res)=>{
     try{
-        const {username , email , password } = 
+        const {username , email , password } = req.body;
+        if(!username || !email || !password){
+            return res.status(400).json({
+                success : false ,
+                message : "username or email or password not provided"
+            });
+        }
+        const emailExist = await User.findOne({email});
+        if(emailExist){
+            return res.status(403).json({
+                success : false ,
+                message  : "email already exist , try with another email"
+            });
+        }
+        const salt =  await bcrypt.genSalt(10);
+        const hashpassword =  await bcrypt.hash(password , salt);
+        
+        const newUser = await User({
+            username ,
+            email , 
+            password : hashpassword
+        });
+        const saveUser = await newUser.save();
+        if(!saveUser){
+            return res.status(402).json({
+                success : false ,
+                message : "User cannot be registered"
+            });
+        }return res.status(200).json({
+            success : true ,
+            message : "User registered successfully"
+        })
     }
     catch(err){
         console.log(err)
     }
 }
+
+
+export {registerUser}
--- Backend/server.js
+++ Backend/server.js
@@ -1,8 +1,9 @@
 import express, { urlencoded } from "express";
 import dotenv from "dotenv";
 import dbConnection from "./database/db.js";
-import userRoutes from "./routes/notice.js"
-import adminRoutes from "./routes/admin.js"
+import userRoutes from "./routes/userRoute.js"
+import adminRoutes from "./routes/adminRoute.js";
+import authRoutes from "./routes/authRoutes.js"
 dotenv.config();
 const PORT = process.env.PORT ;
 
@@ -16,6 +17,7 @@ app.use(urlencoded({extended : true}));
 
 app.use("/api/v1/user",userRoutes);
 app.use("/api/v1/admin",adminRoutes);
+app.use("/api/v1/auth",authRoutes);
 
 
 app.listen(PORT, ()=>{
