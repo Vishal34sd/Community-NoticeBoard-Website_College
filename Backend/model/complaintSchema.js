f60be011313d05ccdb797927ab62b5eee71b6ef5
vishal-07 <dubeyvishal714@gmail.com>
2025-08-04 09:53:23 +0530

    complaint schema added

---
+++ Backend/model/complaintSchema.js
@@ -0,0 +1,29 @@
+import mongoose from 'mongoose';
+
+const complaintSchema = new mongoose.Schema({
+    title : {
+        type : String ,
+        required : true
+    },
+    description : {
+        type : String , 
+        required : true
+    } , 
+    category : {
+        type : String ,
+        enum : ["Garbage" , "Water" , "Electricity" ," Road", "Noise", "Other"],
+        default : "Other"
+    },
+    submittedBy : {
+        type : mongoose.Schema.Types.ObjectId ,
+        ref : "User",
+        required : true
+    },
+    createdAt : {
+        type : Date , 
+        default : Date.now
+    }
+}, {timeStamps : true})
+
+const complaintModel = mongoose.model("Complaint", complaintSchema);
+export default complaintModel;
