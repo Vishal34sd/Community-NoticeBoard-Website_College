0629f087d53b4b8e2c12c97b617c2ff9b1b95282
vishal-07 <dubeyvishal714@gmail.com>
2025-08-04 09:10:22 +0530

    lost and found created

---
+++ Backend/model/lost_foundSchema.js
@@ -0,0 +1,39 @@
+import mongoose from "mongoose";
+
+
+const lostOrfoundSchema = new mongoose.Schema({
+    type :{ String,
+    enum : ["lost" , "found"],
+    required : true},
+    title : {
+        type : String ,
+        required : true
+    },
+    description : {
+        type : String ,
+        required : true 
+    },
+    dateReported : {
+        type : Date ,
+        default : Date.now
+    },
+    dateLostOrFound : {
+        type : Date , 
+        required : true
+    },
+    location : {
+        type : String,
+        required : true
+    },
+    reportedBy : {
+        type : String ,
+        required : true
+    },
+    phone : {
+        type : String ,
+        required : true
+    }
+}, {timestamps : true});
+
+const lostOrFoundModel = mongoose.model("Lost_Found", lostOrfoundSchema);
+export default lostOrFoundModel ;
